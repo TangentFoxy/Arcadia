9e901aad3203048b7e574bb21285bde76405e007
Rose Liverman <paul.liverman.iii@gmail.com>
2020-01-06 16:39:50 -0800

    renamed, automatic docker hub builds

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 set -e # exit on any failure
 
 moonc .
-sudo docker build -t guard13007/realms2:latest .
-#sudo docker push guard13007/realms2:latest
+sudo docker build -t guard13007/arcadia:latest .
+sudo docker push guard13007/arcadia:latest
--- test.sh
+++ test.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 SESSION_SECRET=secret
-DB_HOST=realms2testdb
-DB_USER=realms2test
+DB_HOST=arcadiatestdb
+DB_USER=arcadiatest
 DB_PASS=password
-DB_NAME=realms2test
-NET_NAME=realms2testnet
+DB_NAME=arcadiatest
+NET_NAME=arcadiatestnet
+AC_NAME=arcadiatest
 
-sudo docker stop realms2test
+sudo docker stop $AC_NAME
 sudo docker stop $DB_HOST
-sudo docker rm realms2test
+sudo docker rm $AC_NAME
 sudo docker rm $DB_HOST
 
 sudo docker network create $NET_NAME
@@ -17,8 +18,7 @@ sudo docker run --name $DB_HOST -e POSTGRES_PASSWORD=$DB_PASS \
  -e POSTGRES_USER=$DB_USER -e POSTGRES_DB=$DB_NAME \
  --network $NET_NAME --restart unless-stopped \
  -d postgres:12.1-alpine
-sleep 5 # give it time to spin up to prevent failures on starting my container
-sudo docker run --name realms2test --network $NET_NAME -p 80:80 \
+sudo docker run --name $AC_NAME --network $NET_NAME -p 80:80 \
  -e SESSION_SECRET=$SESSION_SECRET -e DB_HOST=$DB_HOST \
  -e DB_USER=$DB_USER -e DB_PASS=$DB_PASS -e DB_NAME=$DB_NAME \
- --restart unless-stopped -d guard13007/realms2:latest
+ --restart unless-stopped -d guard13007/arcadia:latest
